2214692e9ec255bf4a93f064c7ac9f9c45a36adf
Bogdan Bobir <bogdanbob338@gmail.com>
2018-06-14 13:06:06 +0300

    fixed path to frontent

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/app/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/app/dist', 'index.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
